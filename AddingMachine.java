970091bdc0a3e8a50cc1e37833356eb9793b7d77
Uiseong Sang <yixing93@naver.com>
2019-10-09 02:18:23 -0700

    First submission file

---
+++ AddingMachine.java
@@ -0,0 +1,64 @@
+/*
+ * Name: Uiseong Sang
+ * Class ID: 85141
+ * File: AddingMachine.java
+ * Date: 10/09/19
+ * Assignment number: #2
+ * Description: Using a version control system.
+ *              Also, it is with Javadoc
+ */
+
+package cse360assign2;
+
+/*
+ * Description: This class is just setup for uploadding file to github 
+ *              
+ */
+public class AddingMachine {
+	private int total;
+	
+	/*
+	 * purpose: Conductor
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0; // not needed - included for clarity
+	}
+	
+	/*
+	 * purpose: to get total number.
+	 * 
+	 *  @return 0: initial return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * purpose: to subract number.
+	 * 
+	 * @oaram value: int number for subtract.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * purpose: To display outputs.
+	 * 			string representation of the object.
+	 * 
+	 * @return "": initial return.
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/*
+	 * purpose: to make reset.
+	 * 
+	 */
+	public void clear() {
+		
+	}
+	
+}
