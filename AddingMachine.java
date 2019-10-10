58e5bbdd2a5e01d736df536c83ffa9b73123c089
Uiseong Sang <yixing93@naver.com>
2019-10-09 23:13:25 -0700

    The second upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -11,54 +11,59 @@
 package cse360assign2;
 
 /*
- * Description: This class is just setup for uploadding file to github 
+ * Description: This class is adding and subtracting number
  *              
  */
 public class AddingMachine {
 	private int total;
+	private String history;
 	
 	/*
 	 * purpose: Conductor
-	 * 
+	 * 			initialized every variable
 	 */
 	public AddingMachine () {
-		total = 0; // not needed - included for clarity
+		total = 0; 
+		history = "0";
 	}
 	
 	/*
 	 * purpose: to get total number.
 	 * 
-	 *  @return 0: initial return
+	 *  @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/*
-	 * purpose: to subract number.
+	 * purpose: to add number to total variable.
 	 * 
-	 * @oaram value: int number for subtract.
+	 * param value: integer number for addition
+	 */
+	public void add (int value) {
+		total = total +value;
+		history = history.concat(" + " + value);
+	}
+	
+	/*
+	 * purpose: to subtract number from total variable.
+	 * 
+	 * @param  value: integer number for subtract.
 	 */
 	public void subtract (int value) {
-		
+		history = history.concat(" - " + value);
+		total = total - value;
 	}
+	
 	
 	/*
 	 * purpose: To display outputs.
 	 * 			string representation of the object.
 	 * 
-	 * @return "": initial return.
+	 * @return  history
 	 */
 	public String toString () {
-		return "";
+		return  history;
 	}
-	
-	/*
-	 * purpose: to make reset.
-	 * 
-	 */
-	public void clear() {
-		
-	}
-	
 }
